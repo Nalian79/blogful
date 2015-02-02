3646852c5d78335b6ed9e1708b5f9b899b1b21b7
Michele <michele.morelock@gmail.com>
2015-02-02 02:07:27 +0000

    Added tests for edits and deletes.

--- tests/test_views_integration.py
+++ tests/test_views_integration.py
@@ -54,6 +54,41 @@ class TestViews(unittest.TestCase):
         self.assertEqual(post.content, "<p>Test Content</p>\n")
         self.assertEqual(post.author, self.user)
 
+    def testEditPost(self):
+        self.simulate_login()
+        self.testAddPost()
+        posts = session.query(models.Post).all()
+        self.assertEqual(len(posts), 1)
+        # Get the post we just added.
+
+        post = posts[0]
+        # Call up the edit form
+        edit_post = self.client.post("/post/" + str(post.id) + "/edit", data={
+                "title": "Edited Test Post",
+                "content": "Edited Test Content"
+                })
+
+        # talk to mentor about status code 200 vs 302 here
+        self.assertEqual(edit_post.status_code, 200)
+        posts = session.query(models.Post).all()
+        post = posts[0]
+        self.assertEqual(post.title, "Edited Test Post")
+        self.assertEqual(post.content, "Edited Test Content")
+        self.assertEqual(post.author, self.user)
+
+    def testDeletePost(self):
+        self.simulate_login()
+        self.testAddPost()
+
+        posts = session.query(models.Post).all()
+        self.assertEqual(len(posts), 1)
+
+        post = posts[0]
+
+        rm_post = self.client.post("/post/" + str(post.id) + "/delete", data={})
+        posts = session.query(models.Post).all()
+        self.assertEqual(rm_post.status_code, 200)
+        self.assertEqual(len(posts), 0)
 
 if __name__ == "__main__":
     unittest.main()
