f1726815991c401d152d28f1e4222f994668e339
Michele <michele.morelock@gmail.com>
2015-02-08 18:03:49 +0000

    Removing teardown.

--- blog/__init__.py
+++ blog/__init__.py
@@ -8,10 +8,6 @@ app = Flask(__name__)
 config_path = os.environ.get("CONFIG_PATH", "blog.config.DevelopmentConfig")
 app.config.from_object(config_path)
 
-@app.teardown_appcontext
-def shutdown_session(exception=None):
-    session.remove()
-
 import views
 import filters
 
