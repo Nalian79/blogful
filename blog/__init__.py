dac482c0f1aa4d11dd1ce41fc28b8dfb2ab2e6d6
Michele <michele.morelock@gmail.com>
2015-02-08 18:01:28 +0000

    Adding app teardown.

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,11 +1,16 @@
 import os
 
 from flask import Flask
+from database import session
 
 # ask mentor why this has to come before the below imports.
 app = Flask(__name__)
 config_path = os.environ.get("CONFIG_PATH", "blog.config.DevelopmentConfig")
 app.config.from_object(config_path)
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    session.remove()
 
 import views
 import filters
